92545351a320c83d98b8112c0b2680f6a72c12d5
NyleLuza <jluza21@gmail.com>
2024-02-08 11:07:04 -0800

    comp 333

---
+++ Assignment1/src/Nil.java
@@ -0,0 +1,27 @@
+public class Nil implements ImmutableList {
+    public Nil() {}
+
+    public boolean equals(final Object other) {
+        return other instanceof Nil;
+    } // equals
+
+    public String toString() {
+        return "Nil";
+    } // toString
+
+    public int hashCode() {
+        return 0;
+    } // hashCode
+    public int length(){
+        return 1;
+    }
+    public boolean contains(final int value){
+        return true;
+    }
+    public int sum(){
+        return 1;
+    }
+    public ImmutableList append(ImmutableList other) {
+        return null;
+    }
+} // Nil
